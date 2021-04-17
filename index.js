1b3a017fe2ad8ccdcb4e265ea32f20d9c5e66149
rubelahme <rubelahme3303@gmail.com>
2021-04-18 02:23:21 +0600

    jibon ses

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const express = require("express");
 const app = express();
+require("dotenv").config();
 const MongoClient = require("mongodb").MongoClient;
 const ObjectId = require("mongodb").ObjectId;
 const cors = require("cors");
 const bodyParser = require("body-parser");
-const port = 5000;
 
-const uri =
-  "mongodb+srv://newUserData:VnVLWeG75C0gi6E5@cluster0.obyna.mongodb.net/assinment11?retryWrites=true&w=majority";
+const port = process.env.PORT || 5000;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.obyna.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+console.log(uri);
 app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
@@ -89,7 +90,7 @@ client.connect((err) => {
     collectionAdmin
       .deleteOne({ _id: ObjectId(req.params.id) })
       .then((result) => {
-        console.log(result);
+        res.send(result.deletedCount > 0);
       });
   });
 
